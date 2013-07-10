cd550fc0542ff9fe9cb0ede32d6ebe2aacaf58d3
Stephanie Datu <stephdatu@StephHP.(none)>
2013-07-09 19:24:30 -0700

    Initial commit - Ch. 1 Tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '4d6d715c3abb6d5c2c811adb8b037f0e1ad4497e47c8cea969948d5d233f56279ae2a0030d7e7a5c78aa6d4765d48bd72f066ac3eeac267fa3c7dc383b21822f'
---
+++ db/migrate/20130708215735_create_purchases.rb
@@ -0,0 +1,9 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+      t.string :name 
+      t.float :cost 
+      t.timestamps
+    end
+  end
+end
